abb11a936cf5f87fe4e42836d7c09acc9bde7c9a
MarianaDwarka <mariana_carmona_dwarka@hotmail.com>
2024-04-13 14:12:32 -0600

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import { PatientComponent } from './pages/patient/patient.component';
-import { MedicComponent } from './pages/medic/medic.component';
 
 export const routes: Routes = [
-  { path: 'patient', component: PatientComponent }, //esto solo es un ejemplo
-  { path: 'medic', component: MedicComponent }, //esto solo es un ejemplo
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   {
