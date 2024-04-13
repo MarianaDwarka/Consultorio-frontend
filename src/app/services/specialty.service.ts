fab57a0f66060f63aa89ae376ab26e41992a2270
MarianaDwarka <mariana_carmona_dwarka@hotmail.com>
2024-04-13 17:47:37 -0600

    others

---
+++ src/app/services/specialty.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { GenericService } from './generic.service';
+import { Specialty } from '../model/specialty';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpecialtyService extends GenericService<Specialty> {
+
+  constructor(protected override http: HttpClient) {
+    super(http, `${environment.HOST}/specialties`);
+  }
+
+}
